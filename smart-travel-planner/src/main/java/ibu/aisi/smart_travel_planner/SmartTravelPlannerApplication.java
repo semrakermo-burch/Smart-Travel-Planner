974c3dbdddf8a7cb549a2d054f20ad1d401e328f
semrakermo-burch <kermo.semra@stu.ibu.edu.ba>
2024-12-28 21:21:48 +0100

    Create Spring Boot application

---
+++ smart-travel-planner/src/main/java/ibu/aisi/smart_travel_planner/SmartTravelPlannerApplication.java
@@ -0,0 +1,13 @@
+package ibu.aisi.smart_travel_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTravelPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTravelPlannerApplication.class, args);
+	}
+
+}
